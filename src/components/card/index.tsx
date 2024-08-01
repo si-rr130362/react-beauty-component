73547e162d67e03294e9ea424056aef0df34a3f2
si-dev130362 <rang130362@gmail.com>
2024-08-02 06:13:49 +0900

    color schema updated

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 export default function Card() {
 
   return (
-    <div className="shinobi-card">
+    <div className="si-card">
       <img
         src="https://i.postimg.cc/FRX37FKk/pixlr-image-generator-6f80252f-8fce-4f01-b692-d561fe7663ec.png"
         alt="card"
