5be7115f684498cffdec773b84e2ccd1dfaea9a9
si-dev130362 <rang130362@gmail.com>
2024-08-06 05:46:28 +0900

    Snippet component completed

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,32 +1,64 @@
 interface PropsType {
-    icon: keyof typeof icons;
-    [key: string]: any;
-  }
-  
-  const icons: any = {
-    "arrow-left": (
-      <svg
-        aria-hidden="true"
-        fill="none"
-        focusable="false"
-        height="1em"
-        role="presentation"
-        viewBox="0 0 24 24"
-        width="1em"
-      >
-        <path
-          d="M15.5 19l-7-7 7-7"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1.5"
-        ></path>
-      </svg>
-    ),
-  };
-  
-  const Icon = ({ icon, ...rest }: PropsType) => {
-    return <div {...rest}>{icons[icon]}</div>;
-  };
-  
-  export default Icon;
+  icon: keyof typeof icons;
+  [key: string]: any;
+}
+
+const icons: any = {
+  "arrow-left": (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height="1em"
+      role="presentation"
+      viewBox="0 0 24 24"
+      width="1em"
+    >
+      <path
+        d="M15.5 19l-7-7 7-7"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+      ></path>
+    </svg>
+  ),
+  copy: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+      <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+    </svg>
+  ),
+  doubleCheck: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M18 6 7 17l-5-5" />
+      <path d="m22 10-7.5 7.5L13 16" />
+    </svg>
+  ),
+};
+
+const Icon = ({ icon, ...rest }: PropsType) => {
+  return <div {...rest}>{icons[icon]}</div>;
+};
+
+export default Icon;
