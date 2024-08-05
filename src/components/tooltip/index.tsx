aa62961e97c254600b8b84ccb45839fa3494a1cd
si-dev130362 <rang130362@gmail.com>
2024-08-06 05:38:48 +0900

    updated tooltip component

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 interface TooltipProps {
-  text: string;
-  children: React.ReactNode;
+	text: string;
+	children: React.ReactNode;
 }
 
 const Tooltip: React.FC<TooltipProps> = ({ text, children }) => {
-  return (
-    <div>
-      <span className="tooltip-label">{children}</span>
-      <div className="tooltip">
-        <p>{text}</p>
-      </div>
-    </div>
-  );
+	const [isHovered, setIsHovered] = useState(false);
+
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
+
+	return (
+		<div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+			<span className="tooltip-label">{children}</span>
+			<div className={`tooltip ${isHovered ? "tooltip-hover" : "tooltip-hoverout"}`}>
+				<p>{text}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Tooltip;
